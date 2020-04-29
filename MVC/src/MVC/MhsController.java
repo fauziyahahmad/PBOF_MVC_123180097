eb0c48575333da53652fd62288baf298158c9949
Fauziyah Ahmad Inayanti <61132320+fauziyahahmad@users.noreply.github.com>
2020-04-29 09:33:33 +0700

    Update MhsController.java

--- MVC/src/MVC/MhsController.java
+++ MVC/src/MVC/MhsController.java
@@ -7,7 +7,7 @@ public class MhsController {
     MhsModel mhsmodel;
     MhsView mhsview;
     MhsDAO mhsdao;
-    static String nim;
+    static String dataTerpilih;
     
     public MhsController(MhsModel mhsmodel, MhsView mhsview, MhsDAO mhsdao){
         this.mhsmodel = mhsmodel;
@@ -40,60 +40,30 @@ public class MhsController {
             }
         });
         
-        /* sebenernya mau pake ini tapi masih eror terus bingung benerinnya, ga jadi deh :(
-        mhsview.tabel.addMouseListener(new MouseAdapter() {
-            @Override
+         mhsview.tabel.addMouseListener(new MouseAdapter(){
+           @Override
             public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
                 int baris = mhsview.tabel.getSelectedRow();
                 int kolom = mhsview.tabel.getSelectedColumn();
 
-                String dataTerpilih = mhsview.tabel.getValueAt(baris,1).toString();
-                
-                mhsview.delete.addActionListener((java.awt.event.ActionEvent f) -> {
-                    int input = JOptionPane.showConfirmDialog(null, "Apakah anda ingin menghapus data " + dataTerpilih + "?","Delete Data",JOptionPane.YES_NO_OPTION);
-
-                if (input == 0){
-                    mhsdao.Delete(mhsmodel);
-                    
-                    String dataMahasiswa[][] = mhsdao.readMahasiswa();
-                    mhsview.tabel.setModel(new JTable(dataMahasiswa, mhsview.namaKolom).getModel());
-                }
-                });
-                
-            }
-        });*/
-        
-        mhsview.tabel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                tabelMouseClicked(e);
+                dataTerpilih = mhsview.tabel.getValueAt(baris,0).toString();
                 mhsview.delete.setEnabled(true);
             }
         });
-
-        mhsview.delete.addActionListener(new ActionListener() {
+        
+        mhsview.delete.addActionListener(new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e) {
-                //memasukkan data ke dalam Model
-                mhsmodel.setNim(nim);
-                
-                int input = JOptionPane.showConfirmDialog(null, "Apakah anda ingin menghapus data NIM " + mhsmodel.getNim() + "?","Delete Data",JOptionPane.YES_NO_OPTION);
-                
+            public void actionPerformed(ActionEvent e){
+                int input = JOptionPane.showConfirmDialog(null, "Apakah anda ingin menghapus data NIM " + dataTerpilih + "?","Delete Contact",JOptionPane.YES_NO_OPTION);
+
                 if (input == 0){
-                //menjalankan perintah delete di DAO
-                mhsdao.Delete(mhsmodel);
-                
-                String dataMahasiswa[][] = mhsdao.readMahasiswa();
-                mhsview.tabel.setModel((new JTable(dataMahasiswa, mhsview.namaKolom)).getModel());
+                    mhsmodel.setNim(dataTerpilih);
+                    mhsdao.Delete(mhsmodel);
+                    String dataMahasiswa[][] = mhsdao.readMahasiswa();
+                    mhsview.tabel.setModel((new JTable (dataMahasiswa, mhsview.namaKolom)).getModel());
                 }
             }
         });
         
-    }
-    
-    private void tabelMouseClicked(MouseEvent e) {
-        int baris = mhsview.tabel.rowAtPoint(e.getPoint());
-        nim = mhsview.tabel.getValueAt(baris,0).toString();
     }
 }
